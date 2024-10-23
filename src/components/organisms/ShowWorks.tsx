07a8f2660b70b3baa09348fa898bed1f7197d267
HirokiMiyakura <miyakurahiroki@gmail.com>
2024-10-24 04:23:20 +0900

    third commit

--- src/components/organisms/ShowWorks.tsx
+++ src/components/organisms/ShowWorks.tsx
@@ -3,27 +3,6 @@ import Link from "next/link";
 
 export default async function ShowWorks() {
   const works = await fetchWorks();
-  const settings = {
-    dots: true, // ドットナビゲーションを表示する
-    infinite: true, // ループするかどうか
-    speed: 500, // スライドの速度 (ms)
-    slidesToShow: 3, // 一度に表示するスライドの数
-    slidesToScroll: 1, // 一度にスクロールするスライドの数
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
 
   return (
     <>
